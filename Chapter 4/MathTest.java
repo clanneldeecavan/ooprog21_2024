b2359599ad92de7c5445ad266e84b00f561f30bc
Clannel Dee Cavan <cavanclanneldee@gmail.com>
2024-10-04 09:36:47 +0800

    MathTest.java

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 public class MathTest 
 {
-    public static void main(String[] args) 
+    public static void main(String[] a) 
     {
-        Scanner scanner = new Scanner(System.in); 
-
         System.out.println("SQRT: " + Math.sqrt(37));
         System.out.println("SIN: " + Math.sin(300));
         System.out.println("COS: " + Math.cos(300));
@@ -14,13 +10,6 @@ public class MathTest
         System.out.println("ROUND: " + Math.round(22.7));
         System.out.println("MAX: " + Math.max(71, 68)); //num 1, num2
         System.out.println("MIN: " + Math.min(71, 68)); //num 1, num 2
-        
-        System.out.printf("Enter the maximum value for random number: ");
-        double max = scanner.nextDouble(); 
-
-        double randomValue = Math.random() * max;
-                System.out.println("RANDOM: " + randomValue); 
-
-        scanner.close(); 
-    }
+        System.out.println("RANDOM: " + Math.random()); 
+  }
 }
